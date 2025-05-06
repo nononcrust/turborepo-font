b6ecaa0272f4a6cf6f08b837be60e6fe2dc741b9
nonon <dev.nonon@gmail.com>
2025-05-06 11:14:27 +0900

    예제 수정

--- packages/font/src/index.ts
+++ packages/font/src/index.ts
@@ -3,27 +3,27 @@ import localFont from "next/font/local";
 export const pretendard = localFont({
   src: [
     {
-      path: "../Pretendard-Regular.subset.woff2",
+      path: "./assets/Pretendard-Regular.subset.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../Pretendard-Medium.subset.woff2",
+      path: "./assets/Pretendard-Medium.subset.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../Pretendard-SemiBold.subset.woff2",
+      path: "./assets/Pretendard-SemiBold.subset.woff2",
       weight: "600",
       style: "normal",
     },
     {
-      path: "../Pretendard-Bold.subset.woff2",
+      path: "./assets/Pretendard-Bold.subset.woff2",
       weight: "700",
       style: "normal",
     },
     {
-      path: "../Pretendard-ExtraBold.subset.woff2",
+      path: "./assets/Pretendard-ExtraBold.subset.woff2",
       weight: "800",
       style: "normal",
     },
